df36fd62adbb27a278e745abcc210b5ae6e58ed8
Tong Wu <wutong0910@foxmail.com>
2022-02-18 23:07:02 +0800

    homemarking

--- weapp-homemarking/components/tabs/index.js
+++ weapp-homemarking/components/tabs/index.js
@@ -1,16 +1,16 @@
-import { throttle } from "../../utils/utils";
+import { throttle } from '../../utils/utils';
 
-const moment = require("../../lib/moment")
+const moment = require('../../lib/moment');
 
 Component({
     options: {
-        multipleSlots: true
+        multipleSlots: true,
     },
     properties: {
         tabs: Array,
         active: {
             type: Number,
-            value: 0
+            value: 0,
         },
     },
     data: {
@@ -18,54 +18,51 @@ Component({
     },
     lifetimes: {
         attached() {
-            this.data.lastClickTime = moment()
-        }
+            this.data.lastClickTime = moment();
+        },
     },
     observers: {
         active: function (value) {
             this.setData({
-                currentTabIndex: this.data.active
-            })
-        }
+                currentTabIndex: this.data.active,
+            });
+        },
     },
     methods: {
         handleSwitchTab: function (event) {
-            const index = event.currentTarget.dataset.index
+            const index = event.currentTarget.dataset.index;
             if (this.data.currentTabIndex === index) {
-                const now = moment()
+                const now = moment();
                 const diff = now.diff(this.data.lastClickTime);
                 if (diff < 250) {
-                    this.triggerEvent('doubleclicktab')
+                    this.triggerEvent('doubleclicktab');
                 }
-                this.data.lastClickTime = now
-                return
+                this.data.lastClickTime = now;
+                return;
             }
             throttle(() => {
-                this.setData({
-                    currentTabIndex: index,
-                })
-                this.triggerEvent('change', { index })
-            })()
+                this.setData({ currentTabIndex: index });
+                this.triggerEvent('change', { index });
+            })();
         },
         handleTouchmove: function (event) {
-            const direction = event.direction
-            console.log(1)
+            const direction = event.direction;
 
-            const currentTabIndex = this.data.currentTabIndex
-            const targetTabIndex = currentTabIndex + direction
+            const currentTabIndex = this.data.currentTabIndex;
+            const targetTabIndex = currentTabIndex + direction;
 
             if (targetTabIndex < 0 || targetTabIndex > this.data.tabs.length - 1) {
-                return
+                return;
             }
 
             const customEvent = {
                 currentTarget: {
                     dataset: {
-                        index: targetTabIndex
-                    }
-                }
-            }
-            this.handleSwitchTab(customEvent)
-        }
-    }
+                        index: targetTabIndex,
+                    },
+                },
+            };
+            this.handleSwitchTab(customEvent);
+        },
+    },
 });
--- weapp-homemarking/pages/plaza/index.js
+++ weapp-homemarking/pages/plaza/index.js
@@ -5,15 +5,17 @@ const serviceModel = new Service();
 
 Page({
     data: {
+        loading: true,
         tabs: ['全部服务', '在提供', '正在找'],
         multiple: 0,
         categoryList: [],
         serviceList: [],
         showStatus: false,
     },
-    onLoad() {
-        this.getCategoryList();
-        this.getServiceList();
+    async onLoad() {
+        await this.getCategoryList();
+        await this.getServiceList();
+        this.setData({ loading: false });
     },
     async getCategoryList() {
         const categoryList = await Category.getCategoryListWithAll();
@@ -43,5 +45,16 @@ Page({
     },
     handleScrollToTop: function () {
         wx.pageScrollTo({ scrollTop: 0 });
+    },
+    // 下拉刷新 (需要在 json 里面配置 enablePullDownRefresh)
+    onPullDownRefresh: function () {
+        this.getServiceList(this.data.currentTabIndex, this.data.currentCategoryId);
+        wx.stopPullDownRefresh();
+    },
+    //上拉加载更多
+    onReachBottom: async function () {
+        if (!serviceModel.hasMoreData) return;
+        const serviceList = await serviceModel.getServiceList(this.data.currentTabIndex, this.data.currentCategoryId);
+        this.setData({ serviceList });
     },
 });
